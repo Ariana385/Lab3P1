5e6ba960d44d2f0a6950ce85ea48b8b0e9cb98de
Torj C.F. Ariana-Maria <cti22c111@cst.uoradea.local>
2023-10-30 17:42:13 +0200

    Initial commit

---
+++ src/Lab3P1/Lab3P1.java
@@ -0,0 +1,26 @@
+package Lab3P1;
+import java.util.Scanner;
+public class Lab3P1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Introduceti un sir de caractere: ");
+        String str = in.nextLine();
+        System.out.print("Caracterul din mijlocul sirului: " + middle(str)+"\n");
+    }
+    public static String middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 -1;
+            length = 2;
+    }
+       else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+       return str.substring(position, position + length);
+}
+}
